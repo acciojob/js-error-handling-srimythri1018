b824a274be91147395bf2506365ba4699c5f24d6
srimythri1018 <115878123+srimythri1018@users.noreply.github.com>
2023-04-26 07:40:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,15 @@
-//your code here
 class OutOfRangeError extends Error {
-constructor(arg) {
-super(Expression should only consist of integers and +-/* characters and not ${arg});
-this.name = "OutOfRangeError";
-}
+  constructor(arg) {
+    const message = `Expression should only consist of integers and +-/* characters and not ${arg}`;
+    super(message);
+    this.name = this.constructor.name;
+  }
 }
 
 class InvalidExprError extends Error {
-constructor() {
-super("Expression should not have an invalid combination of expression");
-this.name = "InvalidExprError";
+  constructor() {
+    const message = `Expression should not have an invalid combination of expression`;
+    super(message);
+    this.name = this.constructor.name;
+  }
 }
-}
-
-function evaluateExpression(expression) {
-// Check for invalid characters
-const validChars = /[0-9+-*/\s]/g;
-if (!validChars.test(expression)) {
-throw new OutOfRangeError(expression);
-}
-
-// Check for invalid expressions
-const invalidExprs = /([+-/]{2,}|^\s[+-/]\s|\s+[+-/]\s$)/g;
-if (invalidExprs.test(expression)) {
-throw new InvalidExprError();
-}
-
-// Evaluate the expression
-return eval(expression);
-}
-
-// Example usage
-try {
-const result = evaluateExpression("2 + 3 * 4 - 5 / 2");
-console.log(result);
-} catch (error) {
-console.error(error);
-}
-
-
-
-
-
-
